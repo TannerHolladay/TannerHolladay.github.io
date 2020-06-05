7e4fdedec6019e5e89604d0bee3f434233b87231
Tanner Holladay <TannerHolladay6@gmail.com>
2020-06-05 16:27:23 -0600

    Updates

---
+++ precache-manifest.3070643e748f127d9b0205f3a3bbfc04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de07f257a676a8775ab997b96b970e6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc8dbff10cae19bcf8b2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a29dee43d552d46a46f",
+    "url": "/static/js/2.fd954b0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd954b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc8dbff10cae19bcf8b2",
+    "url": "/static/js/main.a534caec.chunk.js"
+  },
+  {
+    "revision": "ce21a477b3279256273d",
+    "url": "/static/js/runtime-main.c0207fa1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
