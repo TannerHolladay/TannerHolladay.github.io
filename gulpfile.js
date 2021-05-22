86c893b6f20a8155022817ace212524b450ee256
TannerHolladay <tannerholladay6@gmail.com>
2021-05-22 17:00:16 -0600

    Finished creating new website

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,97 @@
-var gulp = require('gulp');
-var csso = require('gulp-csso');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var plumber = require('gulp-plumber');
-var cp = require('child_process');
-var imagemin = require('gulp-imagemin');
-var browserSync = require('browser-sync');
+const gulp = require('gulp');
+const csso = require('gulp-csso');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const cp = require('child_process');
+const log = require('fancy-log');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync');
 
-var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'bundle';
+const jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'bundle';
 
 /*
  * Build the Jekyll Site
  * runs a child process in node that runs the jekyll commands
  */
-gulp.task('jekyll-build', function (done) {
-	return cp.spawn(jekyllCommand, ['exec', 'jekyll', 'build'], {stdio: 'inherit'}).on('close', done);
+gulp.task('jekyll-build', (done) => {
+    return cp.spawn(jekyllCommand, ['exec', 'jekyll', 'build'], {stdio: 'inherit'}).on('close', done);
 });
 
 /*
  * Rebuild Jekyll & reload browserSync
  */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-	browserSync.reload();
-});
+gulp.task('jekyll-rebuild', gulp.series('jekyll-build', (done) => {
+    browserSync.reload()
+    done();
+}));
 
 /*
  * Build the jekyll site and launch browser-sync
  */
-gulp.task('browser-sync', ['jekyll-build'], function() {
-	browserSync({
-		server: {
-			baseDir: '_site'
-		}
-	});
-});
+gulp.task('browser-sync', gulp.series('jekyll-build', (done) => {
+    browserSync({
+        port: 4000,
+        server: {
+            baseDir: '_site'
+        }
+    })
+    done();
+}));
 
 /*
 * Compile and minify sass
 */
-gulp.task('sass', function() {
-  	gulp.src('src/styles/**/*.scss')
-		.pipe(plumber())
-		.pipe(sass())
-		.pipe(csso())
-		.pipe(gulp.dest('assets/css/'));
+gulp.task('sass', (done) => {
+    log("test");
+    gulp.src('src/styles/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(csso())
+        .pipe(gulp.dest('assets/css/'))
+    done();
 });
 
 /*
 * Compile fonts
 */
-gulp.task('fonts', function() {
-	gulp.src('src/fonts/**/*.{ttf,woff,woff2}')
-		.pipe(plumber())
-		.pipe(gulp.dest('assets/fonts/'));
-})
+gulp.task('fonts', (done) => {
+    gulp.src('src/fonts/**/*.{ttf,woff,woff2}')
+        .pipe(plumber())
+        .pipe(gulp.dest('assets/fonts/'))
+    done();
+});
 
 /*
  * Minify images
  */
-gulp.task('imagemin', function() {
-	return gulp.src('src/img/**/*.{jpg,png,gif}')
-		.pipe(plumber())
-		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-		.pipe(gulp.dest('assets/img/'));
+gulp.task('imagemin', (done) => {
+    return gulp.src('src/img/**/*.{jpg,png,gif}')
+        .pipe(plumber())
+        .pipe(imagemin({optimizationLevel: 3, progressive: true, interlaced: true}))
+        .pipe(gulp.dest('assets/img/')).on('close', done);
 });
 
 /**
  * Compile and minify js
  */
-gulp.task('js', function(){
-	return gulp.src('src/js/**/*.js')
-		.pipe(plumber())
-		.pipe(concat('main.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('assets/js/'))
+gulp.task('js', (done) => {
+    return gulp.src('src/js/**/*.js')
+        .pipe(plumber())
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js/')).on('close', done);
 });
 
-gulp.task('watch', function() {
-	gulp.watch('src/styles/**/*.scss', ['sass', 'jekyll-rebuild']);
-	gulp.watch('src/js/**/*.js', ['js']);
-	gulp.watch('src/fonts/**/*.{tff,woff,woff2}', ['fonts']);
-	gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
-	gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-rebuild']);
+gulp.task('watch', (done) => {
+    gulp.watch('./src/js/**/*.js', {usePolling: true}, gulp.series('js', 'jekyll-rebuild'));
+    gulp.watch('./src/styles/**/*.scss', {usePolling: true}, gulp.series('sass', 'jekyll-rebuild'));
+    gulp.watch('./src/fonts/**/*.{tff,woff,woff2}', {usePolling: true}, gulp.series('fonts'));
+    gulp.watch('./src/img/**/*.{jpg,png,gif}', {usePolling: true}, gulp.series('imagemin'));
+    gulp.watch(['./*html', './_includes/*html', './_layouts/*.html'], {usePolling: true}, gulp.series('jekyll-rebuild'));
+    gulp.watch('./_config.yml', {usePolling: true}, gulp.series('jekyll-rebuild'));
+    done();
 });
 
-gulp.task('default', ['js', 'sass', 'fonts', 'browser-sync', 'watch']);
+gulp.task('default', gulp.series('js', 'sass', 'fonts', 'browser-sync', 'watch'));
--- src/js/app.js
+++ src/js/app.js
@@ -1,117 +1,140 @@
-/* sweetScroll load */
-document.addEventListener("DOMContentLoaded", function () {
-  const sweetScroll = new SweetScroll({/* some options */});
+document.addEventListener("DOMContentLoaded", () => {
+    new SweetScroll({easing: 'easeOutQuad'});
+    const BUBBLE_COUNT = (window.innerWidth + window.innerHeight) / 8,
+        BUBBLE_SIZE = 5,
+        BUBBLE_MIN_SCALE = 0.1
 
-  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS('particles-js', {
-    "particles": {
-      "number": {
-        "value": 160,
-        "density": {
-          "enable": true,
-          "value_area": 500
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 1,
-        "random": true,
-        "anim": {
-          "enable": true,
-          "speed": 1,
-          "opacity_min": 0,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 4,
-          "size_min": 0.3,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": false,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 1,
-        "direction": "none",
-        "random": true,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 600
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": false,
-          "mode": "bubble"
-        },
-        "onclick": {
-          "enable": false,
-          "mode": "repulse"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 250,
-          "size": 0,
-          "duration": 2,
-          "opacity": 0,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 400,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true
-  });
+    const canvas = document.querySelector('canvas'),
+        context = canvas.getContext('2d');
 
+    let width,
+        height;
+
+    let app = [];
+
+    let pointerX,
+        pointerY;
+
+    let velocity = {x: 0, y: -.35, tx: 0, ty: 0};
+
+    let touchInput = false;
+
+    generate();
+    resize();
+    step();
+
+    window.onresize = resize;
+    canvas.onmousemove = onMouseMove;
+    canvas.ontouchmove = onTouchMove;
+    canvas.ontouchend = onMouseLeave;
+    document.onmouseleave = onMouseLeave;
+
+    function generate() {
+
+        for (let i = 0; i < BUBBLE_COUNT; i++) {
+            app.push({
+                x: 0,
+                y: 0,
+                z: BUBBLE_MIN_SCALE + Math.random() * (1 - BUBBLE_MIN_SCALE)
+            });
+        }
+
+    }
+
+    function placeBubble(bubble) {
+        bubble.x = Math.random() * width;
+        bubble.y = Math.random() * height;
+    }
+
+    function resize() {
+        width = window.innerWidth + 5;
+        height = window.innerHeight + 5;
+
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        app.forEach(placeBubble);
+    }
+
+    function step() {
+
+        context.clearRect(0, 0, width, height);
+
+        update();
+        render();
+
+        requestAnimationFrame(step);
+
+    }
+
+    function update() {
+        velocity.tx *= 0.6;
+        velocity.ty *= 0.6;
+
+        velocity.x += (velocity.tx - velocity.x) * 0.01;
+        velocity.y += (velocity.ty - velocity.y) * 0.01;
+
+
+        app.forEach((bubble) => {
+            bubble.y -= Math.pow(1 - bubble.z / BUBBLE_SIZE, 10) * .5;
+            bubble.x = (((((bubble.x + velocity.x * bubble.z) + 5) % width) + width) % width) - 5;
+            bubble.y = (((((bubble.y + velocity.y * bubble.z) + 5) % height) + height) % height) - 5;
+        });
+
+    }
+
+    function render() {
+
+        app.forEach((bubble) => {
+
+            context.fillStyle = "#c1c1c1";
+            context.beginPath();
+            const size = BUBBLE_SIZE * bubble.z;
+            context.fillRect(bubble.x, bubble.y, size, size)
+            //context.arc(bubble.x, bubble.y, BUBBLE_SIZE * bubble.z, 0, 2 * Math.PI, true);
+        });
+
+    }
+
+    function movePointer(x, y) {
+
+        if (typeof pointerX === 'number' && typeof pointerY === 'number') {
+
+            let ox = x - pointerX,
+                oy = y - pointerY;
+
+            velocity.tx = velocity.tx + (ox / 8) * (touchInput ? 1 : -1);
+            velocity.ty = velocity.ty + (oy / 8) * (touchInput ? 1 : -1);
+
+        }
+
+        pointerX = x;
+        pointerY = y;
+
+    }
+
+    function onMouseMove(event) {
+
+        touchInput = false;
+
+        movePointer(event.clientX, event.clientY);
+
+    }
+
+    function onTouchMove(event) {
+
+        touchInput = true;
+
+        movePointer(event.touches[0].clientX, event.touches[0].clientY, true);
+
+        event.preventDefault();
+
+    }
+
+    function onMouseLeave() {
+
+        pointerX = null;
+        pointerY = null;
+
+    }
 }, false);
